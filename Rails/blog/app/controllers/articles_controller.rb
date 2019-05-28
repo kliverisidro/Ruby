cf563d9f624956094f2a2e843e6b67394cfe2acc
Ubuntu <ubuntu@ip-172-31-47-252.us-east-2.compute.internal>
2019-05-28 02:22:47 +0000

    Strong params y vista Show

--- Rails/blog/app/controllers/articles_controller.rb
+++ Rails/blog/app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show]
   def new
     @article=Article.new
   end
@@ -20,5 +21,8 @@ class ArticlesController < ApplicationController
   private
   def article_params
     params.require(:article).permit(:title, :body)
+  end
+  def set_article
+    @article=Article.find(params[:id])
   end
 end
