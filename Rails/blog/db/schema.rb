837c88e32dc983f6d4d0d31122ee633bbc2479c8
Ubuntu <ubuntu@ip-172-31-47-252.us-east-2.compute.internal>
2019-06-01 03:26:45 +0000

    005/365 Se aplico scaffold, css, scss, js y se comienza a ver bootstrap  #365Days

--- Rails/blog/db/schema.rb
+++ Rails/blog/db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_27_025455) do
+ActiveRecord::Schema.define(version: 2019_05_30_235100) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "color"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
