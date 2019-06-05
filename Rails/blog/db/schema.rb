ea40daf9a5811eb2c63636eb5aa5e247a2e30f93
Ubuntu <ubuntu@ip-172-31-47-252.us-east-2.compute.internal>
2019-06-05 06:32:31 +0000

    009/365 Se agrega notificaciones bootstrap, instala la gema devise, crea el modelo user, implementa sesion y agrega links de login y registro

--- Rails/blog/db/schema.rb
+++ Rails/blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_30_235100) do
+ActiveRecord::Schema.define(version: 2019_06_05_052942) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -24,6 +24,24 @@ ActiveRecord::Schema.define(version: 2019_05_30_235100) do
     t.string "color"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "name"
+    t.string "last_name"
+    t.string "hobby"
+    t.integer "permission_level", default: 0
+    t.string "uid"
+    t.string "provider"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
